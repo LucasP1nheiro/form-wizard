2ab5125b52f548cfe6ceaa1b3b7b2d7683d13ea2
Lucas P1nheiro <lps1704@gmail.com>
2024-01-29 11:12:19 -0300

    add favicon and logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/header'
 import { ThemeProvider } from '@/components/theme-provider'
+import icon from '@/app/favicon.ico'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href={icon.src} />
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,28 @@
 import Link from 'next/link'
 import React from 'react'
 import { ThemeToggle } from './theme-toggle'
+import Image from 'next/image'
+import logo from '@/assets/logo.svg'
+import { Button } from '@/components/ui/button'
 
 const Header = () => {
   return (
-    <header className="fixed bg-background border-b border-secondary w-screen top-0 h-[10%] text-primary flex items-center">
+    <header className="fixed bg-background border-b border-secondary w-screen top-0 py-4 text-primary flex items-center">
       <nav className="w-full">
         <ul className="flex w-4/5 mx-auto items-center justify-between">
           <li>
-            <Link href="/">Home</Link>
+            <Button asChild size={'icon'} variant={'ghost'} className="p-1">
+              <Link href="/">
+                <Image
+                  src={logo.src}
+                  alt="Logo image"
+                  width={50}
+                  height={50}
+                  priority
+                  quality={100}
+                />
+              </Link>
+            </Button>
           </li>
 
           <li>
