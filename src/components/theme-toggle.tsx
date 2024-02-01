3ab45a31fb97fcdab80ea5c5b1500fad8e8a3332
Lucas P1nheiro <lps1704@gmail.com>
2024-02-01 12:03:20 -0300

    change icon size on theme switch

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -18,12 +18,12 @@ export function ThemeToggle() {
     >
       <Sun
         className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-        size={24}
+        size={18}
         color="#000"
       />
       <Moon
         className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-        size={24}
+        size={18}
         color="#fff"
       />
       <span className="sr-only">Toggle theme</span>
