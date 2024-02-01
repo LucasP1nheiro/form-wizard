3f95447c89f9c234896887294f2ede7371d81363
Lucas P1nheiro <lps1704@gmail.com>
2024-02-01 13:25:43 -0300

    add protected routes on middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,11 +7,17 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
   const supabase = createMiddlewareClient({ req, res })
 
-  await supabase.auth.getSession()
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if (!session) {
+    return NextResponse.rewrite(new URL('/sign-in', req.url))
+  }
 
   return res
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sign-up).*)'],
 }
