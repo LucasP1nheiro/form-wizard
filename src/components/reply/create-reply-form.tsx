0835a2ea413be73a3945f01db172b42edf5691df
Lucas P1nheiro <lps1704@gmail.com>
2024-02-11 16:01:36 -0300

    feat: add name and email as priority fields to create reply

--- src/components/reply/create-reply-form.tsx
+++ src/components/reply/create-reply-form.tsx
@@ -9,14 +9,35 @@ import { Button } from '@/components/ui/button'
 import { ClipLoader } from 'react-spinners'
 import { toast } from 'sonner'
 import { ComponentPropsMap, ComponentProps } from '@/types/components'
-import { FormEvent } from 'react'
+import { Input } from '../ui/input'
+import { Label } from '../ui/label'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Span } from 'next/dist/trace'
+import { cn } from '@/lib/utils'
 
 interface CreateReplyFormProps {
   fields: ComponentProps<keyof ComponentPropsMap>[]
   formId: number
 }
 
+const replyFormSchema = z.object({
+  name: z.string().min(3, 'This field has to have at least 3 characters.'),
+  email: z.string().email('Email is not valid.'),
+})
+
+type ReplyFormSchema = z.infer<typeof replyFormSchema>
+
 const CreateReplyForm = ({ formId, fields }: CreateReplyFormProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ReplyFormSchema>({
+    resolver: zodResolver(replyFormSchema),
+  })
+
   const router = useRouter()
   const { replyFields, setReplyFields } = useReplyFieldsStore()
 
@@ -34,19 +55,55 @@ const CreateReplyForm = ({ formId, fields }: CreateReplyFormProps) => {
     },
   })
 
-  const handleCreateReply = async (e: FormEvent) => {
-    e.preventDefault()
-    const answers = JSON.stringify(replyFields)
+  const handleCreateReply = async (data: ReplyFormSchema) => {
+    if (!errors.root) {
+      const { name, email } = data
 
-    await createReplyFn({ answers, formId })
+      const answers = JSON.stringify(replyFields)
+
+      await createReplyFn({
+        answers,
+        formId,
+        name,
+        email,
+      })
+    }
   }
 
   return (
     <form
       id="form"
       className="lg:w-2/3 w-full space-y-14"
-      onSubmit={handleCreateReply}
+      onSubmit={handleSubmit(handleCreateReply)}
     >
+      <div className="space-y-4">
+        <Label htmlFor="name">Name</Label>
+        <Input
+          id="name"
+          placeholder="Enter your name here"
+          {...register('name')}
+          className={cn({
+            'ring-2 ring-red-500 focus-visible:ring-red-500': errors.name,
+          })}
+        />
+        {errors.name && (
+          <p className="text-red-500 my-4">{errors.name.message}</p>
+        )}
+      </div>
+      <div className="space-y-4">
+        <Label htmlFor="email">Email</Label>
+        <Input
+          id="email"
+          placeholder="Enter your email here"
+          {...register('email')}
+          className={cn({
+            'ring-2 ring-red-500 focus-visible:ring-red-500': errors.email,
+          })}
+        />
+        {errors.email && (
+          <p className="text-red-500 my-4">{errors.email.message}</p>
+        )}
+      </div>
       {fields.map((field) => (
         <FormReplyFields key={field.id} field={field} />
       ))}
